af978e8907f308a5921dbc18c4f5d1cf9cdc3789
julieldg <ludjulie@gmail.com>
2021-12-16 11:23:03 +0100

    fg

--- interface.py
+++ interface.py
@@ -1,5 +1,4 @@
 from tkinter import *
-
 fenetre = Tk()
 photo = PhotoImage(file="backgroundimage.png")
 
@@ -18,14 +17,16 @@ bouton=Button(fenetre, text="Fermer", command=fenetre.quit)
 bouton.pack()
 
 # canvas
-canvas = Canvas(fenetre, width=700, height=500, background='yellow').pack(side=TOP, padx=5, pady=5)
-#canvas.create_image(0, 0, anchor=NW, image=photo)
-Button(fenetre, text ='Niveau précedent').pack(side=LEFT, padx=5, pady=5)
-Button(fenetre, text ='Niveau suivant').pack(side=RIGHT, padx=5, pady=5)
-#canvas.pack()
+canvas = Canvas(fenetre, width=photo.width(), height=photo.height(), background="white")
+canvas.create_image(0, 0, anchor=NW, image=photo)
+#rectangle=canvas.create_oval(25,50,25,50,fill='red')
+Button(fenetre, text ='Niveau précedent').pack(side='left', padx=5, pady=5)
+Button(fenetre, text ='Niveau suivant').pack(side='right', padx=5, pady=5)
+canvas.pack()
 
-class alien():
-    canvas.create_rectangle()
+#class alien():
+   # def __init__(self):
+    #   self.create_rectangle()
 #class jeu():
 
         
