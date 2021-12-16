31d7c270e39186611ec9be44b4845e7e31359375
julieldg <ludjulie@gmail.com>
2021-12-16 09:58:03 +0100

    p

--- interface.py
+++ interface.py
@@ -1,7 +1,9 @@
 from tkinter import *
 
-
 fenetre = Tk()
+photo = PhotoImage(file="backgroundimage.png")
+
+
 
 label = Label(fenetre, text="Bienvenue dans Space Invader", bg="#d39fce")
 label.pack()
@@ -16,9 +18,14 @@ bouton=Button(fenetre, text="Fermer", command=fenetre.quit)
 bouton.pack()
 
 # canvas
-canvas = Canvas(fenetre, width=700, height=500, background='yellow')
-canvas.pack()
+canvas = Canvas(fenetre, width=700, height=500, background='yellow').pack(side=TOP, padx=5, pady=5)
+#canvas.create_image(0, 0, anchor=NW, image=photo)
+Button(fenetre, text ='Niveau précedent').pack(side=LEFT, padx=5, pady=5)
+Button(fenetre, text ='Niveau suivant').pack(side=RIGHT, padx=5, pady=5)
+#canvas.pack()
 
+class alien():
+    canvas.create_rectangle()
 #class jeu():
 
  #   def __init__(self) :
