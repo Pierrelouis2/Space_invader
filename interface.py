e05ebd1e9059ca9293c1f3c2053b65e534932ed7
julieldg <ludjulie@gmail.com>
2021-12-16 09:27:54 +0100

    Canvas

--- interface.py
+++ interface.py
@@ -1,9 +1,29 @@
 from tkinter import *
 
 
-class jeu():
+fenetre = Tk()
 
-    def __init__(self) :
+label = Label(fenetre, text="Bienvenue dans Space Invader", bg="#d39fce")
+label.pack()
+
+
+
+bouton = Checkbutton(fenetre, text="Nouveau?")
+bouton.pack()
+
+# bouton de sortie
+bouton=Button(fenetre, text="Fermer", command=fenetre.quit)
+bouton.pack()
+
+# canvas
+canvas = Canvas(fenetre, width=700, height=500, background='yellow')
+canvas.pack()
+
+#class jeu():
+
+ #   def __init__(self) :
         
 
-        
+
+
+fenetre.mainloop()
