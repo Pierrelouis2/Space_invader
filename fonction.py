0b6e2ef872922c3b2de7fff4b87aa6ff204642cb
julieldg <ludjulie@gmail.com>
2022-01-06 08:34:31 +0100

    class

--- fonction.py
+++ fonction.py
@@ -3,19 +3,28 @@ import time
 
 
 class Entity() :
-    def __init__(self,vie,coord,forme) :
+    def __init__(self,vie,coord,forme,canvas) :
         self.coord = coord #position de l'entité au début
         self.forme = forme #forme de l'entité
         self.life = vie #vie de l'entité
+        self.canvas=canvas
 
+class Monstre():
+    def __init__(self):
 
-    def path_monster(self,xmax,ymax,vitesse,dir) : #xmax, ymax limites du canvas #fait droite ou gauche en fonction de dir
-            if self.coord[0] + dir*vitesse <= xmax or self.coord[0] + dir*vitesse >= 0 : #déplacement droite
-                self.coord[0] += dir*vitesse
-                time.sleep(0.5)
-                print(self.coord)
-                return(dir*vitesse)
         
+    def path_monster(self,xmax,ymax,vitesse,dir) : #xmax, ymax limites du canvas #fait droite ou gauche en fonction de dir
+        global liste_enemy,dir
+    
+        print(canvas.coords(liste_enemy[len(liste_enemy)-1])[2],"cpppr",canvas.winfo_reqwidth())
+        if canvas.coords(liste_enemy[len(liste_enemy)-1])[2] > canvas.winfo_reqwidth() -20 :
+            dir = -5
+            print(dir)
+        elif canvas.coords(liste_enemy[0])[2] < 30: 
+            dir = 5
+        for i in liste_enemy :
+            canvas.move(i,dir,0)
+        fen.after(50,mouv)
         
                     
 
