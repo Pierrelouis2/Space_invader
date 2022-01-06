8f48ab6577a1d6a5bf5c32b7b9b9ccd4e4a7ce6d
julieldg <ludjulie@gmail.com>
2022-01-06 09:07:27 +0100

    fdjik

--- fonction.py
+++ fonction.py
@@ -1,36 +1,42 @@
 import time
 from tkinter.constants import TRUE
+from tkinter import *
 
 
 
 class entity() :
-    def __init__(self,vie,coord,forme,canvas) :
+    def __init__(self,vie,coord,canvas,nom_image) :
         self.coord = coord #position de l'entité au début
-        self.forme = forme #forme de l'entité
         self.life = vie #vie de l'entité
         self.canvas=canvas
+        self.nom_image=nom_image
+    def create(self):
+        self.photo=tk.PhotoImage(file=self.nom_image)
+        self.canvas.create_rectangle(x1,y1,x2,y2,fill="red")
+        x1+= 50
+        x2 += 50
+        print(x2)
 
 class monstre():
-    def __init__(self):
 
         
     def path_monster(self,xmax,ymax,vitesse,dir) : #xmax, ymax limites du canvas #fait droite ou gauche en fonction de dir
         global liste_enemy,dir
     
-        print(canvas.coords(liste_enemy[len(liste_enemy)-1])[2],"cpppr",canvas.winfo_reqwidth())
-        if canvas.coords(liste_enemy[len(liste_enemy)-1])[2] > canvas.winfo_reqwidth() -20 :
+        print(self.canvas.coords(self.liste_enemy[len(self.liste_enemy)-1])[2],"cpppr",self.canvas.winfo_reqwidth())
+        if self.canvas.coords(self.liste_enemy[len(self.liste_enemy)-1])[2] > self.canvas.winfo_reqwidth() -20 :
             dir = -5
             print(dir)
-        elif canvas.coords(liste_enemy[0])[2] < 30: 
+        elif self.canvas.coords(self.liste_enemy[0])[2] < 30: 
             dir = 5
-        for i in liste_enemy :
-            canvas.move(i,dir,0)
+        for i in self.liste_enemy :
+            self.canvas.move(i,dir,0)
         fen.after(50,mouv)
         
                     
 class joueur(entity):
 
- TRUE
+ 
 
 
 
